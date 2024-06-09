c39dab7eb29dbea5c104604e758080abd35a4550
Nic Barr <code@nicholasbarr.net>
2024-06-09 14:40:30 -0500

    start cleanup from where coding left off.

--- src/Providers/WeatherServiceProvider.php
+++ src/Providers/WeatherServiceProvider.php
@@ -22,10 +22,12 @@
         private function setUpNwsServices(): void
         {
             $this->app->singleton(NwsWeatherService::class, function () {
-                $config = new WeatherConfiguration();
+                $config = new WeatherConfiguration([
+                    'stationId' => env('NWS_STATION_ID')
+                ]);
                 $client = new NwsApiClient([
-                    'agent' => env('APP_AGENT_NAME', env('APP_NAME')),
-                    'contact' => env('APP_CONTACT_EMAIL')
+                    'agent' => env('NWS_APP_AGENT_NAME', env('APP_NAME')),
+                    'contact' => env('NWS_APP_CONTACT_EMAIL')
                 ]);
                 return new NwsWeatherService($client, $config);
             });
